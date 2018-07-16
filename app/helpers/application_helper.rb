e4598fc70be2a0b4c4d7c759bd5ebd2f9525ab2f
Jidesh Gopalan <Jidesh.Gopalan@Gmail.com>
2018-07-16 14:23:35 +0530

    css image changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -80,14 +80,14 @@ module ApplicationHelper
   def avatar_thumb(size, person, avatar_html_options={})
     return "" if person.nil?
     image_url = person.site_logo if !person.site_logo.nil?
-    image_url = (person.image.present? ? person.image.url(size) : missing_avatar(size)) if !image_url.nil?
+    image_url = (person.image.present? ? person.image.url(size) : missing_avatar(size)) if image_url.nil?
 
     link_to_unless(person.deleted?, image_tag(image_url, avatar_html_options), person)
   end
 
   def large_avatar_thumb(person, options={})
     image_url = person.site_logo if !person.site_logo.nil?
-    image_url = (person.image.present? ? person.image.url(:medium) : missing_avatar(:medium))   if !image_url.nil?
+    image_url = (person.image.present? ? person.image.url(:medium) : missing_avatar(:medium))   if image_url.nil?
 
     image_tag image_url, { :alt => PersonViewUtils.person_display_name(person, @current_community) }.merge(options)
   end
@@ -96,7 +96,7 @@ module ApplicationHelper
     # FIXME! Need a new picture size: :large
 
     image_url = person.site_logo if !person.site_logo.nil?
-    image_url = (person.image.present? ? person.image.url(:medium) : missing_avatar(:medium)) if !image_url.nil?
+    image_url = (person.image.present? ? person.image.url(:medium) : missing_avatar(:medium)) if image_url.nil?
 
     image_tag image_url, { :alt => PersonViewUtils.person_display_name(person, @current_community) }.merge(options)
   end
